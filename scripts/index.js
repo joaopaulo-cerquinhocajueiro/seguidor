8f23b33f91a6709c258631a00672573ee64dd172
João Paulo Cerquinho Cajueiro <joaopaulo.cerquinhocajueiro@gmail.com>
2020-03-29 22:06:48 -0300

    Adicionada janela de ajuda

--- scripts/index.js
+++ scripts/index.js
@@ -72,6 +72,286 @@ VariablePanel = React.createClass({
   }
 });
 
+var preCode =  "#include <iostream>\n";
+preCode += "using namespace std;\n"
+//preCode += "#include <OrangutanLCD.h>\n";
+//preCode += "#include <OrangutanBuzzer.h>\n";
+//preCode += "#include <OrangutanPushbuttons.h>\n";
+//preCode += "#include <OrangutanAnalog.h>\n";
+//preCode += "#include <OrangutanLEDs.h>\n";
+//preCode += "#include <OrangutanMotors.h>\n";
+preCode += "#define IR_EMITTERS_ON 1\n";
+preCode += "#define BUTTON_A 0\n";
+preCode += "#define BUTTON_B 1\n";
+preCode += "#define BUTTON_C 2\n";
+preCode += "#define LOW 0\n";
+preCode += "#define HIGH 1\n";
+preCode += "OrangutanLCD2 OrangutanLCD;\n";
+preCode += "OrangutanBuzzer2 OrangutanBuzzer;\n";
+preCode += "OrangutanPushbuttons2 OrangutanPushbuttons;\n";
+preCode += "OrangutanAnalog2 OrangutanAnalog;\n";
+preCode += "OrangutanLEDs2 OrangutanLEDs;\n";
+preCode += "OrangutanMotors2 OrangutanMotors;\n\n";
+preCode += "void delay(int t){\nint a = t;\n/*TODO*/}\n";
+
+var result = result = `/*
+* Simple3piLineFollower - demo code for the Pololu 3pi Robot
+* 
+* This code will follow a black line on a white background, using a
+* very simple algorithm.  It demonstrates auto-calibration and use of
+* the 3pi IR sensors, motor control, bar graphs using custom
+* characters, and music playback, making it a good starting point for
+* developing your own more competitive line follower.
+*
+* http://www.pololu.com/docs/0J21
+* http://www.pololu.com
+* http://forum.pololu.com
+*
+*/
+
+// The 3pi include file must be at the beginning of any program that
+// uses the Pololu AVR library and 3pi.  Pololu3pi.h includes all of the
+// other Orangutan Arduino libraries that can be used to control the
+// on-board hardware such as LCD, buzzer, and motor drivers.
+#include <Pololu3pi.h>
+#include <PololuQTRSensors.h>
+#include <OrangutanMotors.h>
+#include <OrangutanAnalog.h>
+#include <OrangutanLEDs.h>
+#include <OrangutanLCD.h>
+#include <OrangutanPushbuttons.h>
+#include <OrangutanBuzzer.h>
+
+Pololu3pi robot;
+unsigned int sensors[5]; // an array to hold sensor values
+
+// This include file allows data to be stored in program space.  The
+// ATmega168 has 16k of program space compared to 1k of RAM, so large
+// pieces of static data should be stored in program space.
+#include <avr/pgmspace.h>
+
+// Introductory messages.  The "PROGMEM" identifier causes the data to
+// go into program space.
+const char welcome_line1[] PROGMEM = " Pololu";
+const char welcome_line2[] PROGMEM = "3\xf7 Robot";
+const char demo_name_line1[] PROGMEM = "Line";
+const char demo_name_line2[] PROGMEM = "follower";
+
+// A couple of simple tunes, stored in program space.
+const char welcome[] PROGMEM = ">g32>>c32";
+const char go[] PROGMEM = "L16 cdegreg4";
+
+// Data for generating the characters used in load_custom_characters
+// and display_readings.  By reading levels[] starting at various
+// offsets, we can generate all of the 7 extra characters needed for a
+// bargraph.  This is also stored in program space.
+const char levels[] PROGMEM = {
+ 0b00000,
+ 0b00000,
+ 0b00000,
+ 0b00000,
+ 0b00000,
+ 0b00000,
+ 0b00000,
+ 0b11111,
+ 0b11111,
+ 0b11111,
+ 0b11111,
+ 0b11111,
+ 0b11111,
+ 0b11111
+};
+
+// This function loads custom characters into the LCD.  Up to 8
+// characters can be loaded; we use them for 7 levels of a bar graph.
+void load_custom_characters()
+{
+ OrangutanLCD::loadCustomCharacter(levels + 0, 0); // no offset, e.g. one bar
+ OrangutanLCD::loadCustomCharacter(levels + 1, 1); // two bars
+ OrangutanLCD::loadCustomCharacter(levels + 2, 2); // etc...
+ OrangutanLCD::loadCustomCharacter(levels + 3, 3);
+ OrangutanLCD::loadCustomCharacter(levels + 4, 4);
+ OrangutanLCD::loadCustomCharacter(levels + 5, 5);
+ OrangutanLCD::loadCustomCharacter(levels + 6, 6);
+ OrangutanLCD::clear(); // the LCD must be cleared for the characters to take effect
+}
+
+// This function displays the sensor readings using a bar graph.
+void display_readings(const unsigned int *calibrated_values)
+{
+ unsigned char i;
+
+ for (i=0;i<5;i++) {
+   // Initialize the array of characters that we will use for the
+   // graph.  Using the space, an extra copy of the one-bar
+   // character, and character 255 (a full black box), we get 10
+   // characters in the array.
+   const char display_characters[10] = { ' ', 0, 0, 1, 2, 3, 4, 5, 6, 255 };
+
+   // The variable c will have values from 0 to 9, since
+   // calibrated values are in the range of 0 to 1000, and
+   // 1000/101 is 9 with integer math.
+   char c = display_characters[calibrated_values[i] / 101];
+
+   // Display the bar graph character.
+   OrangutanLCD::print(c);
+ }
+}
+
+// Initializes the 3pi, displays a welcome message, calibrates, and
+// plays the initial music.  This function is automatically called
+// by the Arduino framework at the start of program execution.
+void setup()
+{
+ unsigned int counter; // used as a simple timer
+
+ // This must be called at the beginning of 3pi code, to set up the
+ // sensors.  We use a value of 2000 for the timeout, which
+ // corresponds to 2000*0.4 us = 0.8 ms on our 20 MHz processor.
+ robot.init(2000);
+
+ load_custom_characters(); // load the custom characters
+
+ // Play welcome music and display a message
+ OrangutanLCD::printFromProgramSpace(welcome_line1);
+ OrangutanLCD::gotoXY(0, 1);
+ OrangutanLCD::printFromProgramSpace(welcome_line2);
+ OrangutanBuzzer::playFromProgramSpace(welcome);
+ delay(1000);
+
+ OrangutanLCD::clear();
+ OrangutanLCD::printFromProgramSpace(demo_name_line1);
+ OrangutanLCD::gotoXY(0, 1);
+ OrangutanLCD::printFromProgramSpace(demo_name_line2);
+ delay(1000);
+
+ // Display battery voltage and wait for button press
+ while (!OrangutanPushbuttons::isPressed(BUTTON_B))
+ {
+   int bat = OrangutanAnalog::readBatteryMillivolts();
+
+   OrangutanLCD::clear();
+   OrangutanLCD::print(bat);
+   OrangutanLCD::print("mV");
+   OrangutanLCD::gotoXY(0, 1);
+   OrangutanLCD::print("Press B");
+
+   delay(100);
+ }
+
+ // Always wait for the button to be released so that 3pi doesn't
+ // start moving until your hand is away from it.
+ OrangutanPushbuttons::waitForRelease(BUTTON_B);
+ delay(1000);
+
+ // Auto-calibration: turn right and left while calibrating the
+ // sensors.
+ for (counter=0; counter<80; counter++)
+ {
+   if (counter < 20 || counter >= 60)
+     OrangutanMotors::setSpeeds(40, -40);
+   else
+     OrangutanMotors::setSpeeds(-40, 40);
+
+   // This function records a set of sensor readings and keeps
+   // track of the minimum and maximum values encountered.  The
+   // IR_EMITTERS_ON argument means that the IR LEDs will be
+   // turned on during the reading, which is usually what you
+   // want.
+   robot.calibrateLineSensors(IR_EMITTERS_ON);
+
+   // Since our counter runs to 80, the total delay will be
+   // 80*20 = 1600 ms.
+   delay(20);
+ }
+ OrangutanMotors::setSpeeds(0, 0);
+
+ // Display calibrated values as a bar graph.
+ while (!OrangutanPushbuttons::isPressed(BUTTON_B))
+ {
+   // Read the sensor values and get the position measurement.
+   unsigned int position = robot.readLine(sensors, IR_EMITTERS_ON);
+
+   // Display the position measurement, which will go from 0
+   // (when the leftmost sensor is over the line) to 4000 (when
+   // the rightmost sensor is over the line) on the 3pi, along
+   // with a bar graph of the sensor readings.  This allows you
+   // to make sure the robot is ready to go.
+   OrangutanLCD::clear();
+   OrangutanLCD::print(position);
+   OrangutanLCD::gotoXY(0, 1);
+   display_readings(sensors);
+
+   delay(100);
+ }
+ OrangutanPushbuttons::waitForRelease(BUTTON_B);
+
+ OrangutanLCD::clear();
+
+ OrangutanLCD::print("Go!");       
+
+ // Play music and wait for it to finish before we start driving.
+ OrangutanBuzzer::playFromProgramSpace(go);
+ while(OrangutanBuzzer::isPlaying());
+}
+
+// The main function.  This function is repeatedly called by
+// the Arduino framework.
+void loop()
+{
+ // Get the position of the line.  Note that we *must* provide
+ // the "sensors" argument to read_line() here, even though we
+ // are not interested in the individual sensor readings.
+ unsigned int position = robot.readLine(sensors, IR_EMITTERS_ON);
+ if (position < 1000)
+ {
+   // We are far to the right of the line: turn left.
+
+   // Set the right motor to 100 and the left motor to zero,
+   // to do a sharp turn to the left.  Note that the maximum
+   // value of either motor speed is 255, so we are driving
+   // it at just about 40% of the max.
+   OrangutanMotors::setSpeeds(0, 100);
+
+   // Just for fun, indicate the direction we are turning on
+   // the LEDs.
+   OrangutanLEDs::left(HIGH);
+   OrangutanLEDs::right(LOW);
+ }
+ else if (position < 3000)
+ {
+   // We are somewhat close to being centered on the line:
+   // drive straight.
+   OrangutanMotors::setSpeeds(100, 100);
+   OrangutanLEDs::left(HIGH);
+   OrangutanLEDs::right(HIGH);
+ }
+ else
+ {
+   // We are far to the left of the line: turn right.
+   OrangutanMotors::setSpeeds(100, 0);
+   OrangutanLEDs::left(LOW);
+   OrangutanLEDs::right(HIGH);
+ }
+}`;
+//result += "void simPrint(char t){/*TODO*/}\n";
+//result += "void simPrint(char* t){/*TODO*/}\n";
+//result += "void simPrint(int t){/*TODO*/}\n";
+//result += fr.result;
+result = result.replace(/::/g, ".");
+result = result.replace(/ PROGMEM/g, "");
+result = result.replace(/\\xf7 /g, "pi");
+result = result.replace(/OrangutanLCD.print\(( |[A-Z]|[a-z]|'|[0-9]|"|!)*\);/g, ""); // remove as chamadas de print pois não há overloading implementado
+//result = result.replace(/while\(OrangutanBuzzer::isPlaying\(\)\);/g, "");
+result = result.replace("while(OrangutanBuzzer.isPlaying());", ""); // por algum motivo ele nao entende a construcao while(algo); // tive que remover
+var postCode = "\nint main() {\n";
+postCode += "\tsetup();\n";
+postCode += "\twhile(true){\n";
+postCode += "\t\tloop();\n";
+postCode += "\t}\n";
+postCode += "\treturn 0;\n";
+postCode += "}\n";
+
 Main = React.createClass({
   displayName: "Main",
   getInitialState: function() {
@@ -85,7 +365,8 @@ Main = React.createClass({
       lastVars: []
     };
   },
-  defaultCode: "#include <iostream>\nusing namespace std;\nint main() {\n    int a;\n    cin >> a;\n    cout << a*10 << endl;\n    return 0;\n}",
+  //defaultCode: "#include <iostream>\nusing namespace std;\nint main() {\n    int a;\n    cin >> a;\n    cout << a*10 << endl;\n    return 0;\n}",
+  defaultCode: result,
   componentDidMount: function() {
     jQuery.hotkeys.options.filterInputAcceptingElements = false;
     jQuery.hotkeys.options.filterContentEditable = false;
@@ -120,7 +401,9 @@ Main = React.createClass({
   run: function(debug, e) {
     var code, config, exitCode, input;
     e.preventDefault();
-    code = this.state.code;
+    code = preCode;
+    code += this.state.code;
+    code += postCode;
     input = this.state.input;
     this.output = "";
     config = {
@@ -145,6 +428,7 @@ Main = React.createClass({
     if (debug) {
       this.preDebug();
       try {
+        console.log(code);
         this["debugger"] = JSCPP.run(code, input, config);
         return this.startDebug();
       } catch (error) {
@@ -329,45 +613,14 @@ Main = React.createClass({
       fr = new FileReader();
       fr.onloadend = (function(_this) {
 		return function() {
-			var result = "#include <iostream>\n";
-          result += "using namespace std;\n"
-          result += "#include <OrangutanLCD.h>\n";
-          result += "#include <OrangutanBuzzer.h>\n";
-          result += "#include <OrangutanPushbuttons.h>\n";
-          result += "#include <OrangutanAnalog.h>\n";
-          result += "#include <OrangutanLEDs.h>\n";
-          result += "#include <OrangutanMotors.h>\n";
-          result += "#define IR_EMITTERS_ON 1\n";
-          result += "#define BUTTON_A 0\n";
-          result += "#define BUTTON_B 1\n";
-          result += "#define BUTTON_C 2\n";
-          result += "#define LOW 0\n";
-          result += "#define HIGH 1\n";
-          result += "OrangutanLCD2 OrangutanLCD;\n";
-          result += "OrangutanBuzzer2 OrangutanBuzzer;\n";
-          result += "OrangutanPushbuttons2 OrangutanPushbuttons;\n";
-          result += "OrangutanAnalog2 OrangutanAnalog;\n";
-          result += "OrangutanLEDs2 OrangutanLEDs;\n";
-          result += "OrangutanMotors2 OrangutanMotors;\n\n";
-          result += "void delay(int t){\nint a = t;\n/*TODO*/}\n";
-          //result += "void simPrint(char t){/*TODO*/}\n";
-          //result += "void simPrint(char* t){/*TODO*/}\n";
-          //result += "void simPrint(int t){/*TODO*/}\n";
-          result += fr.result;
+			var result = fr.result;
           result = result.replace(/::/g, ".");
           result = result.replace(/ PROGMEM/g, "");
           result = result.replace(/\\xf7 /g, "pi");
           result = result.replace(/OrangutanLCD.print\(( |[A-Z]|[a-z]|'|[0-9]|"|!)*\);/g, ""); // remove as chamadas de print pois não há overloading implementado
           //result = result.replace(/while\(OrangutanBuzzer::isPlaying\(\)\);/g, "");
           result = result.replace("while(OrangutanBuzzer.isPlaying());", ""); // por algum motivo ele nao entende a construcao while(algo); // tive que remover
-          result += "\nint main() {\n";
-          result += "\tsetup();\n";
-          result += "\twhile(true){\n";
-          result += "\t\tloop();\n";
-          result += "\t}\n";
-          result += "\treturn 0;\n";
-          result += "}\n";
-
+          
           //console.log(result);      
       		//fr.result = result;
         
@@ -447,7 +700,7 @@ Main = React.createClass({
     //   "glyph": "floppy-open"
     // }), "Quick Open (Ctrl + O)"),
     React.createElement(MenuItem, {
-      "eventKey": "quick-save"
+      "eventKey": "loadRoad"
     }, React.createElement(Glyphicon, {
       "glyph": "road"
     }), " Pista..."),
@@ -481,7 +734,18 @@ Main = React.createClass({
       "disabled": !editing
     }, React.createElement(Glyphicon, {
       "glyph": "sunglasses"
-    }), " Simulation")
+    }), " Simulation"),
+    React.createElement(NavItem, {
+      "onClick": (function(){
+        x = document.getElementById("helpScreen");
+        if(x.style.display === "none"){
+          x.style.display = "block";
+        } else{
+          x.style.display = "none";
+        }}),
+    }, React.createElement(Glyphicon, {
+      "glyph": "question-sign"
+    }), " Ajuda")
     )), React.createElement(Grid, null, (debugging ? React.createElement(Row, {
       "className": "debug-toolbar"
     },) : void 0), React.createElement(Row, {
